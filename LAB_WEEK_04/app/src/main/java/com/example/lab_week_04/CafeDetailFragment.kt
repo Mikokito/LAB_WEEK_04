06d856b34958fc98c75eb55c684de79a60b245a6
Miko <citranandariani@gmail.com>
2024-09-23 11:20:16 +0700

    Update CafeDetailFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -11,3 +11,5 @@ private const val TAB_CONTENT = "TAB_CONTENT"
 
 class CafeDetailFragment : Fragment() {
     private var content: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
