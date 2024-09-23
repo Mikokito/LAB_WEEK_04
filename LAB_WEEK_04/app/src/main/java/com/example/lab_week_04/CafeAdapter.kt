10393affede3007d8a135c9d23475917f4116f09
dep1102 <vincentiusdevon9@gmail.com>
2024-09-23 11:18:33 +0700

    Update CafeAdapter.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,1 +1,7 @@
 package com.example.lab_week_04
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
