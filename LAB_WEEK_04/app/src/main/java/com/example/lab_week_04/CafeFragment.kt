bdfd74fb8ffa975c79a2710706a109331a08a3c3
Miko <citranandariani@gmail.com>
2024-09-23 11:08:48 +0700

    Update CafeFragment.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -9,3 +9,4 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
+class CafeFragment : Fragment() {
