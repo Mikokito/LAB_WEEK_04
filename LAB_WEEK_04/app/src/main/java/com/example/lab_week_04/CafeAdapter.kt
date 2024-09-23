0f102883c69f9dbbb22b0be0cc3c82150528a4de
dep1102 <vincentiusdevon9@gmail.com>
2024-09-23 11:18:56 +0700

    Update CafeAdapter.kt

--- LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ LAB_WEEK_04/app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -5,3 +5,5 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+
+val TABS_FIXED = listOf(
